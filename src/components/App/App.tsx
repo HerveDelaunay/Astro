e73331cd304e3c851179ea7a6bfc7792ce4efeec
HerveDelaunay <delaunay.herve06@gmail.com>
2022-07-27 18:41:32 +0200

    setup done

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Mercury from '../Mercury/Mercury';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Mercury/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mercury/Mercury.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mercury = () => {
+  return (
+    <div>Mercury</div>
+  )
+}
+
+export default Mercury
