12a1dffaa8bc67c5ae30f872fcba722bfb19f53f
HerveDelaunay <delaunay.herve06@gmail.com>
2022-08-09 14:15:23 +0200

    desktop version done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,31 +2,59 @@ import Header from '../Header/Header';
 import Planet from '../Planet/Planet';
 import PlanetStats from '../PlanetStats/PlanetStats';
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import data from '../../assets/data.json'
 
 const App: React.FC = () => {
   const [planet, setPlanet] = useState<number>(0)
   const [internalStructure, setInternalStructure] = useState<boolean>(false)
   const [surfaceGeology, setSurfaceGeology] = useState<boolean>(false)
   const [overview, setOverview] = useState<boolean>(true)
+  const slug = window.location.pathname.slice(1)
 
+  useEffect(() => {
+    if(planet) return
+    const slugToIndex = data.findIndex((planet) => planet.name === slug)
+    setPlanet(slugToIndex)
+  }, [slug])
+  
   return (
     <div className="App">
-      <Header setPlanet={setPlanet} 
-        planetId={planet} 
-        setInternalStructure={setInternalStructure} 
-        setSurfaceGeology={setSurfaceGeology}
-        setOverview={setOverview}
+       <Header setPlanet={setPlanet} 
+          planetId={planet} 
+          setInternalStructure={setInternalStructure} 
+          setSurfaceGeology={setSurfaceGeology}
+          setOverview={setOverview}
         />
-      <Planet planetId={planet} 
-        internalStructure={internalStructure} 
-        setInternalStructure={setInternalStructure} 
-        surfaceGeology= {surfaceGeology} 
-        setSurfaceGeology={setSurfaceGeology}
-        overview={overview}
-        setOverview={setOverview}
-        />
-      <PlanetStats planetId={planet}/>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Planet planetId={planet} 
+              internalStructure={internalStructure} 
+              setInternalStructure={setInternalStructure} 
+              surfaceGeology= {surfaceGeology} 
+              setSurfaceGeology={setSurfaceGeology}
+              overview={overview}
+              setOverview={setOverview}
+            />
+            <PlanetStats planetId={planet}/>
+          </>
+        }/>
+        <Route path={`${data[planet].name}`} element={
+          <>
+            <Planet planetId={planet} 
+              internalStructure={internalStructure} 
+              setInternalStructure={setInternalStructure} 
+              surfaceGeology= {surfaceGeology} 
+              setSurfaceGeology={setSurfaceGeology}
+              overview={overview}
+              setOverview={setOverview}
+            />
+            <PlanetStats planetId={planet}/>
+          </>
+        }/>
+      </Routes>
     </div>
   );
 }
