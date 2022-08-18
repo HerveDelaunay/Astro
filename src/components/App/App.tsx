4b6a305677b09c30284e0b51360f5de03ccc7608
HerveDelaunay <delaunay.herve06@gmail.com>
2022-08-18 14:27:53 +0200

    screenshot adding

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,76 +1,87 @@
-import Header from '../Header/Header';
-import Planet from '../Planet/Planet';
-import PlanetStats from '../PlanetStats/PlanetStats';
-import './App.scss';
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import data from '../../assets/data.json'
-import MobileMenu from '../MobileMenu/MobileMenu';
+import Header from "../Header/Header";
+import Planet from "../Planet/Planet";
+import PlanetStats from "../PlanetStats/PlanetStats";
+import "./App.scss";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import data from "../../assets/data.json";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 const App: React.FC = () => {
-  const [planet, setPlanet] = useState<number>(0)
-  const [internalStructure, setInternalStructure] = useState<boolean>(false)
-  const [surfaceGeology, setSurfaceGeology] = useState<boolean>(false)
-  const [overview, setOverview] = useState<boolean>(true)
-  const [mobileMenuOpened, setMobileMenuOpened] = useState<boolean>(false)
-  const slug = window.location.pathname.slice(1)
+  const [planet, setPlanet] = useState<number>(0);
+  const [internalStructure, setInternalStructure] = useState<boolean>(false);
+  const [surfaceGeology, setSurfaceGeology] = useState<boolean>(false);
+  const [overview, setOverview] = useState<boolean>(true);
+  const [mobileMenuOpened, setMobileMenuOpened] = useState<boolean>(false);
+  const slug = window.location.pathname.slice(1);
 
   useEffect(() => {
-    if(planet) return
-    if(window.location.pathname === "/"){
-      setPlanet(0)
-      return
+    if (planet) return;
+    if (window.location.pathname === "/") {
+      setPlanet(0);
+      return;
     }
-    const slugToIndex = data.findIndex((planet) => planet.name === slug)
-    setPlanet(slugToIndex)
-  })
-  
+    const slugToIndex = data.findIndex((planet) => planet.name === slug);
+    setPlanet(slugToIndex);
+  });
+
   return (
-    <div className={`App ${mobileMenuOpened && 'App--mobile'}`}>
-      <Header setPlanet={setPlanet} 
-          planetId={planet} 
-          setInternalStructure={setInternalStructure} 
-          setSurfaceGeology={setSurfaceGeology}
-          setOverview={setOverview}
-          mobileMenuOpened={mobileMenuOpened}
+    <div className={`App ${mobileMenuOpened && "App--mobile"}`}>
+      <Header
+        setPlanet={setPlanet}
+        planetId={planet}
+        setInternalStructure={setInternalStructure}
+        setSurfaceGeology={setSurfaceGeology}
+        setOverview={setOverview}
+        mobileMenuOpened={mobileMenuOpened}
+        setMobileMenuOpened={setMobileMenuOpened}
+      />
+      {mobileMenuOpened ? (
+        <MobileMenu
           setMobileMenuOpened={setMobileMenuOpened}
+          planetId={planet}
+          setPlanet={setPlanet}
         />
-      {
-        mobileMenuOpened ? (
-          <MobileMenu setMobileMenuOpened={setMobileMenuOpened} planetId={planet} setPlanet={setPlanet}/>
-          ) : (
-      <Routes>
-        <Route path='/' element={
-          <>
-            <Planet planetId={planet} 
-              internalStructure={internalStructure} 
-              setInternalStructure={setInternalStructure} 
-              surfaceGeology= {surfaceGeology} 
-              setSurfaceGeology={setSurfaceGeology}
-              overview={overview}
-              setOverview={setOverview}
-            />
-            <PlanetStats planetId={planet}/>
-          </>
-        }/>
-        <Route path={`${data[planet].name}`} element={
-          <>
-            <Planet planetId={planet} 
-              internalStructure={internalStructure} 
-              setInternalStructure={setInternalStructure} 
-              surfaceGeology= {surfaceGeology} 
-              setSurfaceGeology={setSurfaceGeology}
-              overview={overview}
-              setOverview={setOverview}
-            />
-            <PlanetStats planetId={planet}/>
-          </>
-        }/>
-      </Routes>
-          )
-      }
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Planet
+                  planetId={planet}
+                  internalStructure={internalStructure}
+                  setInternalStructure={setInternalStructure}
+                  surfaceGeology={surfaceGeology}
+                  setSurfaceGeology={setSurfaceGeology}
+                  overview={overview}
+                  setOverview={setOverview}
+                />
+                <PlanetStats planetId={planet} />
+              </>
+            }
+          />
+          <Route
+            path={`${data[planet].name}`}
+            element={
+              <>
+                <Planet
+                  planetId={planet}
+                  internalStructure={internalStructure}
+                  setInternalStructure={setInternalStructure}
+                  surfaceGeology={surfaceGeology}
+                  setSurfaceGeology={setSurfaceGeology}
+                  overview={overview}
+                  setOverview={setOverview}
+                />
+                <PlanetStats planetId={planet} />
+              </>
+            }
+          />
+        </Routes>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -1,104 +1,125 @@
-import React from 'react'
-import './Planet.scss'
-import data from '../../assets/data.json'
-import Props from '../Type'
-import { useState } from 'react'
+import React from "react";
+import "./Planet.scss";
+import data from "../../assets/data.json";
+import Props from "../Type";
+import { useState } from "react";
 
-
-const Planet: React.FC<Props> = ({planetId, internalStructure, setInternalStructure, surfaceGeology, setSurfaceGeology, overview, setOverview}) => {
-  const [internal, setInternal] = useState(false)
-  const [surface, setSurface] = useState(false)
+const Planet: React.FC<Props> = ({
+  planetId,
+  internalStructure,
+  setInternalStructure,
+  surfaceGeology,
+  setSurfaceGeology,
+  overview,
+  setOverview,
+}) => {
+  const [internal, setInternal] = useState(false);
+  const [surface, setSurface] = useState(false);
   const handleClickInternal = () => {
-    setInternalStructure(true)
-    setSurfaceGeology(false)
-    setOverview(false)
-  }
+    setInternalStructure(true);
+    setSurfaceGeology(false);
+    setOverview(false);
+  };
   const handleClickOverview = () => {
-    setInternalStructure(false)
-    setSurfaceGeology(false)
-    setOverview(true)
-  }
+    setInternalStructure(false);
+    setSurfaceGeology(false);
+    setOverview(true);
+  };
   const handleClickSurface = () => {
-    setOverview(false)
-    setInternalStructure(false)
-    setSurfaceGeology(true)
-  }
+    setOverview(false);
+    setInternalStructure(false);
+    setSurfaceGeology(true);
+  };
   return (
     <>
-      { internalStructure ? (
+      {internalStructure ? (
         <div className={`logo__internal logo__internal--${planetId}`}></div>
-        ) : (
-          <div className={`logo logo--${planetId}`}></div>
-        ) 
-      }
-      { surfaceGeology && (
-        <div className={`logo__surface--${planetId}`}></div>
-        )
-      }
-      <section className='planet'>
-        <article className='planet__article'>
-          <div className='planet__title'>{data[planetId].name}</div>
-          <div className='planet__description'>
-            { internalStructure ? data[planetId].structure.content 
-            : surfaceGeology ? data[planetId].geology.content 
-            : data[planetId].overview.content }
+      ) : (
+        <div className={`logo logo--${planetId}`}></div>
+      )}
+      {surfaceGeology && <div className={`logo__surface--${planetId}`}></div>}
+      <section className="planet">
+        <article className="planet__article">
+          <div className="planet__title">{data[planetId].name}</div>
+          <div className="planet__description">
+            {internalStructure
+              ? data[planetId].structure.content
+              : surfaceGeology
+              ? data[planetId].geology.content
+              : data[planetId].overview.content}
           </div>
-          <div className='planet__source'>
-            <div>
-            Source : 
-            </div>
-            <a href=
-            { internalStructure ? data[planetId].structure.source 
-              : surfaceGeology ? data[planetId].geology.source 
-              : data[planetId].overview.source } 
-            className='planet__link'>Wikipedia 
+          <div className="planet__source">
+            <div>Source :</div>
+            <a
+              href={
+                internalStructure
+                  ? data[planetId].structure.source
+                  : surfaceGeology
+                  ? data[planetId].geology.source
+                  : data[planetId].overview.source
+              }
+              className="planet__link"
+            >
+              Wikipedia
             </a>
-            <div className='link__logo'></div>
+            <div className="link__logo"></div>
           </div>
         </article>
-        <div className='planet__buttons'>
-          { overview ? (
-            <button className={`button button--active--${planetId}`} onClick={()=>handleClickOverview()}>
-            <div className='button__number'>01</div>
-            <div className='button__title'>OVERVIEW</div>
+        <div className="planet__buttons">
+          {overview ? (
+            <button
+              className={`button button--active--${planetId}`}
+              onClick={() => handleClickOverview()}
+            >
+              <div className="button__number">01</div>
+              <div className="button__title">OVERVIEW</div>
             </button>
-            ) : (
-            <button className={`button`} onClick={()=>handleClickOverview()}>
-              <div className='button__number'>01</div>
-              <div className='button__title'>OVERVIEW</div>
+          ) : (
+            <button className={`button`} onClick={() => handleClickOverview()}>
+              <div className="button__number">01</div>
+              <div className="button__title">OVERVIEW</div>
             </button>
-            )
-          }
-          {
-            internalStructure ? (
-            <button className={`button button--active--${planetId}`} onClick={() => handleClickInternal()}>
-              <div className='button__number'>02</div>
-              <div className='button__title'><span className='remove__word'>INTERNAL</span> STRUCTURE</div>
+          )}
+          {internalStructure ? (
+            <button
+              className={`button button--active--${planetId}`}
+              onClick={() => handleClickInternal()}
+            >
+              <div className="button__number">02</div>
+              <div className="button__title">
+                <span className="remove__word">INTERNAL</span> STRUCTURE
+              </div>
             </button>
-            ) : (
+          ) : (
             <button className={`button`} onClick={() => handleClickInternal()}>
-              <div className='button__number'>02</div>
-              <div className='button__title'><span className='remove__word'>INTERNAL</span> STRUCTURE</div>
-            </button> 
-            )
-          }
-          { surfaceGeology ? (
-            <button className={`button button--active--${planetId}`} onClick={()=>handleClickSurface()}>
-              <div className='button__number'>03</div>
-              <div className='button__title'>SURFACE <span className='remove__word'>GEOLOGY</span></div>
+              <div className="button__number">02</div>
+              <div className="button__title">
+                <span className="remove__word">INTERNAL</span> STRUCTURE
+              </div>
             </button>
-            ) : (
-            <button className={`button`} onClick={()=>handleClickSurface()}>
-              <div className='button__number'>03</div>
-              <div className='button__title'>SURFACE <span className='remove__word'>GEOLOGY</span></div>
+          )}
+          {surfaceGeology ? (
+            <button
+              className={`button button--active--${planetId}`}
+              onClick={() => handleClickSurface()}
+            >
+              <div className="button__number">03</div>
+              <div className="button__title">
+                SURFACE <span className="remove__word">GEOLOGY</span>
+              </div>
             </button>
-            )
-
-          }
+          ) : (
+            <button className={`button`} onClick={() => handleClickSurface()}>
+              <div className="button__number">03</div>
+              <div className="button__title">
+                SURFACE <span className="remove__word">GEOLOGY</span>
+              </div>
+            </button>
+          )}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Planet
+export default Planet;
